00860b533e9efbd7cea3d042776713d826324552
Arjumnr <arjumnurramadhan@gmail.com>
2024-08-26 01:29:15 +0800

    add template

---
+++ app/Http/Controllers/PencatatanController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PencatatanController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', [AntrianController::class, 'antrian'])->name('antrian');
+// Route::post('/', [AntrianController::class, 'post'])->name('postAntrian');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/authh', [AuthController::class, 'authenticate'])->name('loginPost');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Route::group(['middleware' => ['auth']], function () {
+
+//     Route::group(['middleware' => ['admin:1']], function () {
+//         Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+//         Route::resource('users', UsersController::class);
+//         Route::resource('jenis', JenisController::class);
+//         Route::resource('antrian', AntrianAdminController::class);
+//         Route::resource('barang', BarangController::class);
+//         Route::resource('servis', ServisController::class);
+//         Route::resource('penjualan', PenjualanController::class);
+//         Route::resource('honor', HonorController::class);
+
+
+            
+//     });
+// });
